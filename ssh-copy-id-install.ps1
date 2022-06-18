3bb17b473649df3cb49d736e8d215d876fcaa566
Martin Reinok <78325056+martinreinok@users.noreply.github.com>
2022-06-19 01:27:22 +0300

    Update ssh-copy-id-install.ps1
    Added ExecutionPolicy

--- ssh-copy-id-install.ps1
+++ ssh-copy-id-install.ps1
@@ -1,7 +1,6 @@
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
 if(-not(Test-Path $profile)) { New-Item -path $profile -type file –force | Out-Null}
-
 Add-Content -Path $profile -Value @'
-
 function ssh-copy-id($host_at_address, $ssh_option)
 {
 if(-not($host_at_address)) { Write-Warning "`nPlease include parameter: host@address`nOptionally include ssh_options (eg '-p port')"; Return}
