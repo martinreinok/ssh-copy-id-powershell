3358ec689887eb0cd4f516b49c793d00185e06da
Martin Reinok <78325056+martinreinok@users.noreply.github.com>
2022-06-19 00:53:22 +0300

    Create ssh-copy-id-install.ps1

---
+++ ssh-copy-id-install.ps1
@@ -0,0 +1,11 @@
+if(-not(Test-Path $profile)) { New-Item -path $profile -type file –force | Out-Null}
+
+Add-Content -Path $profile -Value @'
+
+function ssh-copy-id($host_at_address, $ssh_option)
+{
+if(-not($host_at_address)) { Write-Warning "`nPlease include parameter: host@address`nOptionally include ssh_options (eg '-p port')"; Return}
+Get-Content $env:USERPROFILE\.ssh\id_rsa.pub | ssh $host_at_address $ssh_option "cat >> .ssh/authorized_keys"
+}
+
+'@
